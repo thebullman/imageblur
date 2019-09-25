50f36e74d0105d3df87e535a62339823ddc51d79
B. Kyrk Ullman <git@bkullman.com>
2019-09-25 22:44:31 +0000

    Image Blur 1

---
+++ image-blur1.rb
@@ -0,0 +1,20 @@
+class Image
+  def initialize(rows)
+    @rows = rows
+  end
+
+  def output_image
+    @rows.each do |row|
+      puts row.join
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
